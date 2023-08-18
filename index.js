d27dd7d3f4e139a623fd71c6a46e5819b3141118
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-08-18 00:10:40 -0500

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of the project?',
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation?',
+    },
+    {
+        type: 'input',
+        name: 'why',
+        message: 'Why did you build this project?',
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does it solve?',
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: 'What did you learn?',
+    },
+    {
+        type: 'input',
+        name: 'special',
+        message: 'What makes your project stand out?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
