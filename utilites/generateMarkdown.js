6de4cf2579159cfc736108b5472a4d1bdf3db43f
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-08-21 18:36:43 -0500

    created function to return license badge

--- utilites/generateMarkdown.js
+++ utilites/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadge = {
+    mit: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    apache: "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    gnu: "[![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  };
+
+  return licenseBadge[license.toLowerCase()] || " ";
+}
+
+const license = "MIT";
+const badge = renderLicenseBadge(license);
+console.log(badge);
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
